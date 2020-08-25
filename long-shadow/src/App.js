e6604200d8aa3ec1baa37356b20121fd6e276b57
Ramon Snellink <r.snellink@gmail.com>
2020-08-25 11:04:33 +0200

    added comments

--- long-shadow/src/App.js
+++ long-shadow/src/App.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from "react"; // import the useEffect hook
 
 const App = () => {
+  //useEffect accepts 2 params. The first is a callback function. The second WHEN the code should run.
   useEffect(() => {
     const text = document.getElementById("text");
     let shadow = "";
@@ -9,7 +10,11 @@ const App = () => {
       shadow += (shadow ? "," : "") + i * 1 + "px " + i * 1 + "px 0 #b85d1c";
     }
     text.style.textShadow = shadow;
-  }, []);
+  }, []); // [] is the second argument.
+
+  // [] = Run at initial render
+  // [ data ] = Run at initial render AND after re-render and "data" (example name) has changed
+  // empty = Run at initial render AND at every re-render
 
   return <div id="text">Portfolio</div>;
 };
