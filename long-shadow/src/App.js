e60ea4283dbf823b66ca13c0aed04740e5b616c4
Ramon Snellink <r.snellink@gmail.com>
2020-08-25 10:40:06 +0200

    used the useEffect hook to run the setup code once

--- long-shadow/src/App.js
+++ long-shadow/src/App.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function App() {
-   /*    const text = document.getElementById("text");
-   let shadow = "";
+const App = () => {
+  useEffect(() => {
+    const text = document.getElementById("text");
+    let shadow = "";
 
-   for (let i = 0; i < 500; i++) {
+    for (let i = 0; i < 500; i++) {
       shadow += (shadow ? "," : "") + i * 1 + "px " + i * 1 + "px 0 #b85d1c";
-   }
-   text.style.textShadow = shadow; */
+    }
+    text.style.textShadow = shadow;
+  }, []);
 
-   return <div id="text">Portfolio</div>;
-}
+  return <div id="text">Portfolio</div>;
+};
 
 export default App;
