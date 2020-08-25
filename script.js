0a288e3bf9b582a684767c36b75bfbb46275712d
Marco Pauws <mpauws@gmail.com>
2020-08-25 09:08:13 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var text = document.getElementById("text");
+var shadow = "";
+
+for (var i = 0; i < 500; i++) {
+   shadow += (shadow ? "," : "") + i * 1 + "px " + i * 1 + "px 0 #b85d1c";
+}
+text.style.textShadow = shadow;
