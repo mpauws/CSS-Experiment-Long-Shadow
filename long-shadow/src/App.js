63a0e8de008a31f9760a6181916bced0a38a6149
Marco Pauws <mpauws@gmail.com>
2020-08-25 09:34:38 +0200

    convert to react

---
+++ long-shadow/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+   /*    const text = document.getElementById("text");
+   let shadow = "";
+
+   for (let i = 0; i < 500; i++) {
+      shadow += (shadow ? "," : "") + i * 1 + "px " + i * 1 + "px 0 #b85d1c";
+   }
+   text.style.textShadow = shadow; */
+
+   return <div id="text">Portfolio</div>;
+}
+
+export default App;
---
+++ long-shadow/src/vanilla-version/script.js
@@ -0,0 +1,7 @@
+const text = document.getElementById("text");
+let shadow = "";
+
+for (let i = 0; i < 500; i++) {
+   shadow += (shadow ? "," : "") + i * 1 + "px " + i * 1 + "px 0 #b85d1c";
+}
+text.style.textShadow = shadow;
